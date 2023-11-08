6d0802e9177feee6f758224dcaf2a19c6e419e4c
mtworoz <mtworoz@gmail.com>
2023-11-08 22:36:28 +0100

    some primary templates

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +10,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function homepage() : Response
+    public function homepage(CategoryRepository $categoryRepository) : Response
     {
-        return new Response('hello');
+        return $this->render('homepage.html.twig', ['categories' => $categoryRepository->findAll()]);
     }
 
 }
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -20,10 +20,16 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('scripts', './assets/js/app.js')
+    .addStyleEntry('styles', './assets/scss/app.scss')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
+
+    .copyFiles({
+        from: './assets/imgs',
+        to: 'images/[path][name].[hash:8].[ext]'
+    })
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
@@ -54,7 +60,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
