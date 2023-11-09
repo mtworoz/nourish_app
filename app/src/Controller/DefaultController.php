06b6c45043ff120de2da298198324abd51500f66
mtworoz <mtworoz@gmail.com>
2023-11-09 23:04:14 +0100

    post teaser template

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,9 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function homepage(CategoryRepository $categoryRepository) : Response
+    public function homepage(CategoryRepository $categoryRepository, PostRepository $postRepository) : Response
     {
-        return $this->render('homepage.html.twig', ['categories' => $categoryRepository->findAll()]);
+        $recentPosts = $postRepository->findBy([],['date' => 'DESC'],6);
+        return $this->render('homepage.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+            'posts' => $recentPosts
+        ]);
     }
 
 }
