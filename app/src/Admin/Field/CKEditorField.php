26605170a62725c377bcaa9b0d9cfdc2ee162a33
mtworoz <mtworoz@gmail.com>
2023-11-09 18:51:00 +0100

    CKEditor bundle + CKEditorField type

---
+++ app/src/Admin/Field/CKEditorField.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Admin\Field;
+
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FieldTrait;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+
+final class CKEditorField implements FieldInterface
+{
+    use FieldTrait;
+
+    /**
+     * @param string|false|null $label
+     */
+    public static function new(string $propertyName, $label = null): self
+    {
+        return (new self())
+            ->setProperty($propertyName)
+            ->setLabel($label)
+
+            // this template is used in 'index' and 'detail' pages
+            ->setTemplateName('crud/field/text_editor')
+
+            // this is used in 'edit' and 'new' pages to edit the field contents
+            // you can use your own form types too
+            ->setFormType(CKEditorType::class)
+            ->addCssClass('field-ckeditor');
+    }
+}
--- app/src/Controller/Admin/PostCrudController.php
+++ app/src/Controller/Admin/PostCrudController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Admin\Field\CKEditorField;
 use App\Entity\Post;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -17,13 +18,21 @@ class PostCrudController extends AbstractCrudController
         return Post::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        $crud->setFormThemes([
+            '@FOSCKEditor/Form/ckeditor_widget.html.twig',
+            '@EasyAdmin/crud/form_theme.html.twig'
+        ]);
+        return $crud;
+    }
 
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
             TextField::new('title'),
-            TextEditorField::new('content'),
+            CKEditorField::new('content'),
             AssociationField::new('recipes'),
         ];
     }
