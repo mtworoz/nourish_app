08a0fac80a336ffce370a9ebfb604d486628f702
mtworoz <mtworoz@gmail.com>
2023-11-23 23:56:03 +0100

    write comment form - persist entity

--- app/src/Controller/PostController.php
+++ app/src/Controller/PostController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Post;
 use App\Form\WriteCommentFormType;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +15,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends AbstractController
 {
     #[Route('/post/{id}', name: 'single_post')]
-    public function showPost(Post $post): Response
+    public function showPost(Post $post, Request $request, EntityManagerInterface $em): Response
     {
         $commentForm = $this->createForm(WriteCommentFormType::class);
-        
+        $commentForm->handleRequest($request);
+        if($commentForm->isSubmitted() && $commentForm->isValid()){
+            /** @var Comment $comment */
+            $comment = $commentForm->getData();
+            $comment->setPost($post);
+            $comment->setDate(new \DateTime);
+            $em->persist($comment);
+            $em->flush();
+        }
         return $this->render('pages/post.html.twig', [
             'post' => $post,
             'commentForm' => $commentForm
