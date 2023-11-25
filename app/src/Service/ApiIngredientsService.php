b7464f1a35c4baa0c532d499b4707b3a3aa3abbd
mtworoz <mtworoz@gmail.com>
2023-11-25 17:06:08 +0100

    Api service refactor

--- app/src/Service/ApiIngredientsService.php
+++ app/src/Service/ApiIngredientsService.php
@@ -3,22 +3,23 @@
 namespace App\Service;
 
 use Symfony\Component\HttpClient\HttpClient;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ApiIngredientsService
 {
+    private string $apiURL;
+    private HttpClientInterface $client;
+
+    public function __construct()
+    {
+        $this->apiURL = 'http://nginx-service-macromap/api/products';
+        $this->client = HttpClient::create();
+    }
+
     public function get(){
-        $client = HttpClient::create();
-
-        $apiUrl = 'http://nginx-service-macromap/api/products';
-
-        $response = $client->request('GET', $apiUrl);
-
-        $statusCode = $response->getStatusCode();
-
+        $response = $this->client->request('GET', $this->apiURL);
         $data = $response->getContent();
-
-        return new Response(json_encode($data), $statusCode);
+        return json_decode($data)->{'hydra:member'};
     }
 
 }
