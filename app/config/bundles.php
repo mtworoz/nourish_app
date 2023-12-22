2f9ac973318f8402d526c0544966de52684eb081
mtworoz <mtworoz@gmail.com>
2023-12-22 20:50:54 +0100

    Revert "elastic search install"
    This reverts commit a7015befdda483abe2001e7ba3a2e4a14f8c3534.

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -11,5 +11,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
-    FOS\ElasticaBundle\FOSElasticaBundle::class => ['all' => true],
 ];
