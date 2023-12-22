a7015befdda483abe2001e7ba3a2e4a14f8c3534
mtworoz <mtworoz@gmail.com>
2023-12-22 19:31:03 +0100

    elastic search install

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    FOS\ElasticaBundle\FOSElasticaBundle::class => ['all' => true],
 ];
