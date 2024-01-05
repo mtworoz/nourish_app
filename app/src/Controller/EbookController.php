c10cd2a3b9d85ecf668622079bdbb447a92b0a02
mtworoz <mtworoz@gmail.com>
2024-01-05 17:59:52 +0100

    Ebook table of contents + cover

--- app/src/Controller/EbookController.php
+++ app/src/Controller/EbookController.php
@@ -2,9 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Ebook;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class EbookController extends AbstractController
 {
 
+    #[Route('/ebook/{id}', name: 'ebook')]
+    public function showEbook(Ebook $ebook)
+    {
+        return $this->render('elements/ebooks/cover.html.twig', [
+            'ebook' => $ebook
+        ]);
+    }
 }
