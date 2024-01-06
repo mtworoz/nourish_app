f320a9597d383a959074c081a47d52c40a647dc9
Magdalena <mtworoz@gmail.com>
2024-01-06 01:07:52 +0100

    Auto stash before merge of "ebooks" and "origin/ebooks"

--- app/src/Controller/EbookController.php
+++ app/src/Controller/EbookController.php
@@ -3,17 +3,52 @@
 namespace App\Controller;
 
 use App\Entity\Ebook;
+use Knp\Snappy\Pdf;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class EbookController extends AbstractController
 {
 
-    #[Route('/ebook/{id}', name: 'ebook')]
-    public function showEbook(Ebook $ebook)
+    private Environment $twig;
+    private Pdf $pdf;
+    private Filesystem $filesystem;
+
+    public function __construct(Environment $twig, Pdf $pdf, Filesystem $filesystem)
     {
-        return $this->render('elements/ebooks/cover.html.twig', [
+        $this->twig = $twig;
+        $this->pdf = $pdf;
+        $this->filesystem = $filesystem;
+    }
+
+    #[Route('/ebook/{id}', name: 'ebook')]
+    public function showEbook(Ebook $ebook, KernelInterface $kernel)
+    {
+        $html = $this->twig->render('elements/ebooks/cover.html.twig', [
             'ebook' => $ebook
         ]);
+
+        $projectPath = $kernel->getProjectDir();
+
+        $targetDirectory = $projectPath . '/public/ebooks';
+
+        $pdf = $this->pdf->getOutputFromHtml($html);
+
+        $fileName = 'nazwa_pliku.pdf';
+
+        $targetFilePath = $targetDirectory . '/' . $fileName;
+
+        if (!$this->filesystem->exists($targetDirectory)) {
+            $this->filesystem->mkdir($targetDirectory);
+        }
+
+        $this->filesystem->dumpFile($targetFilePath, $pdf);
+
+        return new Response('ok');
     }
+
 }
